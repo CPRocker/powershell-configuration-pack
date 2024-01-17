a46e55a309e56caacf373d5b02fbf175c2b337a8
CPRocker <75814830+CPRocker@users.noreply.github.com>
2024-01-17 12:10:26 -0700

    Create Profile.ps1

---
+++ Profile.ps1
@@ -0,0 +1,19 @@
+# Setup plugins
+oh-my-posh --init --shell pwsh --config "C:\Users\CarsonPrice\dev\source\repos\ohmyposh\config.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+Import-Module posh-git
+
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+$env:PYTHONIOENCODING = 'utf-8'
+Invoke-Expression "$(thefuck --alias)"
+
+# Create aliases
+Set-Alias im Import-Module
+Set-Alias tn Test-NetConnection
+
+Import-Module $PSScriptRoot/Scripts/Open-RepoProject.ps1 -Force
+Clear-Host
